f48d16b45b993b2fb1d96d0ba942608be433d12f
SkyWa7ch3r <joel.g.w.dunstan@gmail.com>
2021-09-23 17:12:31 +0800

    Tests for Histogram are now based on a sav file. Plan to turn into npy or pickle files later.

--- PyFHD/tests/test_fhd_utils/test_histogram.py
+++ PyFHD/tests/test_fhd_utils/test_histogram.py
@@ -1,80 +1,127 @@
 import pytest
 import numpy as np
 from fhd_utils.histogram import histogram
+from scipy.io import readsav
 
-def test_idl_example() :
+def test_idl_example(shared_datadir) :
     """
     This test is based on the example from the IDL documentation.
     This ensures we get the same behaviour as an example everybody can see.
     """
-    # Setup the test
-    data = np.array([[ -5,   4,   2,  -8,   1],
-                    [  3,   0,   5,  -5,   1],
-                    [  6,  -7,   4,  -4,  -8],
-                    [ -1,  -5, -14,   2,   1]])
-
+    # Setup the test from the histogram data file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['idl_hist_example']
     hist, _, indices = histogram(data)
     # Set the expected
-    expected_hist = np.array([1, 0, 0, 0, 0, 0, 2, 1, 0, 3, 1, 0, 0, 1, 1, 3, 2, 1, 2, 1, 1]) 
-    expected_indices = np.array([22, 23, 23, 23, 23, 23, 23, 25, 26, 26, 29, 30, 30, 30, 31, 32, 35,
-                              37, 38, 40, 41, 42, 17,  3, 14, 11,  0,  8, 16, 13, 15,  6,  4,  9,
-                              19,  2, 18,  5,  1, 12,  7, 10])
+    expected_hist = testing_dict['idl_example_hist']
+    expected_indices = testing_dict['idl_example_inds']
     # Assert they are equal element by element
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_one_hundred_nums():
+def test_one_hundred_nums(shared_datadir):
     """
     This is a basic test of an array with numbers 0 to 99 in increasing
     order.
     Should produce two bins.
     """
-    data = np.arange(100)
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     hist, _, indices = histogram(data, bin_size = 50)
-    expected_hist = np.array([50, 50])
-    expected_indices = np.array([3, 53, 103] + list(range(100)))
+    expected_hist = testing_dict['hundred_ints_hist_bin50']
+    expected_indices = testing_dict['hundred_ints_inds_bin50']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_one_hundred_ten_bins():
-    data = np.arange(100)
+def test_one_hundred_ten_bins(shared_datadir):
+     # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     # This is to show that bin_size is ignored when num_bins is used
     hist, _, indices = histogram(data, num_bins = 10, bin_size=1000)
-    expected_hist = np.ones(10) * 10
-    expected_indices = np.array(list(range(11,121, 10)) + list(range(100)))
+    expected_hist = testing_dict['hundred_ints_hist_nbin10']
+    expected_indices = testing_dict['hundred_ints_inds_nbin10']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_min():
-    data = np.arange(100)
+def test_min(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     hist, _, indices = histogram(data, bin_size = 10, min = 10)
-    expected_hist = np.ones(9) * 10
-    expected_indices = np.array(list(range(10,110, 10)) + list(range(10,100)))
+    expected_hist = testing_dict['hundred_ints_hist_min10']
+    expected_indices = testing_dict['hundred_ints_inds_min10']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_max():
-    data = np.arange(100)
+def test_max(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     hist, _, indices = histogram(data, bin_size = 10, max = 50)
-    expected_hist = np.append(np.ones(5) * 10, 1)
-    expected_indices = np.array(list(range(7,67, 10)) + [58] + list(range(0,51)))
+    expected_hist = testing_dict['hundred_ints_hist_max50']
+    expected_indices = testing_dict['hundred_ints_inds_max50']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_min_max():
-    data = np.arange(100)
+def test_min_max(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     hist, _, indices = histogram(data, bin_size = 10, min = 10, max = 55)
-    expected_hist = np.append(np.ones(4) * 10, 6)
-    expected_indices = np.array(list(range(6, 56, 10)) + [52] + list(range(10,56)))
+    expected_hist = testing_dict['hundred_ints_hist_min10_max55']
+    expected_indices = testing_dict['hundred_ints_inds_min10_max55']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
-def test_one_max():
-    data = np.arange(100)
+def test_one_max(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['hundred_ints']
     hist, _, indices = histogram(data, max = 55)
-    expected_hist = np.ones(56) 
-    start = np.size(expected_hist) + 1
-    expected_indices = np.array(list(range(start, start * 2)) + list(range(0,start - 1)))
+    expected_hist = testing_dict['hundred_ints_hist_binsize1_max55']
+    expected_indices = testing_dict['hundred_ints_inds_binsize1_max55']
+    assert np.array_equal(hist, expected_hist)
+    assert np.array_equal(indices, expected_indices)
+
+def test_normals(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['normals']
+    hist, _, indices = histogram(data)
+    expected_hist = testing_dict['normals_hist']
+    expected_indices = testing_dict['normals_inds']
+    assert np.array_equal(hist, expected_hist)
+    assert np.array_equal(indices, expected_indices)
+
+def test_normals_binsize(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['normals']
+    hist, _, indices = histogram(data, bin_size = 0.25)
+    expected_hist = testing_dict['normals_hist_binsize025']
+    expected_indices = testing_dict['normals_inds_binsize025']
+    assert np.array_equal(hist, expected_hist)
+    assert np.array_equal(indices, expected_indices)
+
+def test_normals_min_max(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['normals']
+    hist, _, indices = histogram(data, min = 0, max = 1, bin_size = 0.25)
+    expected_hist = testing_dict['normals_hist_min_max']
+    expected_indices = testing_dict['normals_inds_binsize_min_max']
+    assert np.array_equal(hist, expected_hist)
+    assert np.array_equal(indices, expected_indices)
+
+def test_normals(shared_datadir):
+    # Read the histogram file
+    testing_dict = readsav(shared_datadir / 'histogram.sav', python_dict=True)
+    data = testing_dict['normals'] * 10
+    hist, _, indices = histogram(data, bin_size = 2)
+    expected_hist = testing_dict['normals_hist_times10']
+    expected_indices = testing_dict['normals_inds_times10']
     assert np.array_equal(hist, expected_hist)
     assert np.array_equal(indices, expected_indices)
 
